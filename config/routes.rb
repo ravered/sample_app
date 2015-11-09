6494764e799a5f8a15148661df50d8d6c024b3c8
ravered <ravered@mac.com>
2015-11-09 23:36:57 +0000

    Make a basic User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 SampleApp::Application.routes.draw do
-
-  devise_for :users
+  
+  get "users/show"
+  #devise_for  :users
+  resources :users, only: [:show]
   root  'static_pages#home'
 
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-
-  # devise_for :users
-  resources :users, only: [:show]
-  
 
   
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103231812) do
+ActiveRecord::Schema.define(version: 20151108114203) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
